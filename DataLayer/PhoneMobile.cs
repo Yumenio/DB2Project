6b232555aa6c4421db1c49cbf2818d13d9e0bc6c
Yumenio <andy.castaneda@estudiantes.matcom.uh.cu>
2020-03-05 12:38:51 -0500

    Add project files.

---
+++ DataLayer/PhoneMobile.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DataLayer
+{
+    public class PhoneMobile
+    {
+        // la infle, esto esta aca simplemente como ejemplo, pero es altamente probable que no se use como modelo q:V
+    }
+}
